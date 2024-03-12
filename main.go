6d342c072c2d139db405304cea84ea2364b583f5
maadiab <mohanad.dev@gmail.com>
2024-03-12 19:09:49 +0300

    serve static folder

--- main.go
+++ main.go
@@ -1,46 +1,49 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
-	Database "github.com/maadiab/aldifaArchive/database"
+	Handlers "github.com/maadiab/aldifaArchive/handlers"
 	"github.com/maadiab/aldifaArchive/routes"
 )
 
-func init() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("static", fs))
+// func init() {
+// 	fs := http.FileServer(http.Dir("static"))
+// 	http.Handle("/static/", http.StripPrefix("static", fs))
 
-	storage := http.FileServer(http.Dir("storage"))
-	http.Handle("/storage/", http.StripPrefix("storage", storage))
-}
+// 	storage := http.FileServer(http.Dir("storage"))
+// 	http.Handle("/storage/", http.StripPrefix("storage", storage))
+// }
 
 func main() {
 
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	_, err := Database.ConnectDB(ctx)
+	// _, err := Database.ConnectDB(ctx)
 
-	if err != nil {
-		log.Println("Error Connecting Database !!!", err)
-	}
+	// if err != nil {
+	// 	log.Println("Error Connecting Database !!!", err)
+	// }
 
-	err = Database.CreateUsers()
-	if err != nil {
-		log.Println("main: Error Creating users Table !!!", err)
-	}
+	// err = Database.CreateUsers()
+	// if err != nil {
+	// 	log.Println("main: Error Creating users Table !!!", err)
+	// }
 
-	err = Database.CreatePhotographerTable()
-	if err != nil {
-		log.Println("main: Error Creating photographers Table !!!", err)
-	}
+	// err = Database.CreatePhotographerTable()
+	// if err != nil {
+	// 	log.Println("main: Error Creating photographers Table !!!", err)
+	// }
 
 	r := routes.Router()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/login", Handlers.ServeLogin)
+
 	log.Println("server is running at port: 8080 ...")
 	http.ListenAndServe(":8080", r)
 
-	defer Database.DB.Close()
+	// defer Database.DB.Close()
 
 }
